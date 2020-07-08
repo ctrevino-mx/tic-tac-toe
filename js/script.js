a16cdfc35aec7d1788c1454ff66b654e420bba3d
Cesar Trevino <cesarhtrevino@gmail.com>
2020-07-08 10:38:47 -0500

    Commit: Initial painting logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 console.log('script linked...');
+
+const bodyTicTacToe = document.querySelectorAll('.square');
+let redTurn = true;
+
+function paint(e) {
+    const mySquare2 = document.querySelector('#square1');
+    const mySquare1 = event.target;
+    const classListNames = mySquare1.classList.value;
+    const isThisRed = classListNames.indexOf('Red');
+    const isThisBlue = classListNames.indexOf('Blue');
+
+    if (isThisRed < 0 && isThisBlue < 0) {
+        if (redTurn) {
+            mySquare1.classList.add('squareRed');
+            redTurn = false;    
+        } else {
+            mySquare1.classList.add('squareBlue');
+            redTurn = true;
+        }
+    } 
+}
+
+bodyTicTacToe.forEach(mySquare => {
+    mySquare.addEventListener('click',paint);
+})
